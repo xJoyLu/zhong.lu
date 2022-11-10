53f9e5707aadbbe1b5819f450e345452867000e8
卢东东 <xjoylu@gmail.com>
2022-11-10 16:38:28 +0800

    中路联名上限

--- scripts/sites.js
+++ scripts/sites.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: 卢东东 xjoylu@gmail.com
+ * @Date: 2022-11-10 16:16:15
+ * @LastEditors: 卢东东 xjoylu@gmail.com
+ * @LastEditTime: 2022-11-10 16:27:09
+ * @FilePath: /zhong.lu/scripts/sites.js
+ * @Description: 偷懒界翘楚
+ * @Bio：https://L.Lcc.cc
+ * Copyright (c) 2022 by 卢东东 xjoylu@gmail.com, All Rights Reserved. 
+ */
 'use strict';
 
 // 不要忘了末尾的半角逗号！
@@ -5,17 +15,6 @@
 // {url:"https://abc.xyz", title:"此处输入网站名", type:"可选，此处输入网站类型", author:"可选，此处输入作者",contact:"可选，此处输入网站管理员邮箱", rss:"可选，此处输入网站 RSS"}
 
 const sites = [
-    {url:"https://zhaodao.ai", title:"找到AI", type:"Website", contact:"hi@zhaodao.ai", rss:"https://zhaodao.ai/rss"},
-    {url:"https://capray.blog", title:"Bug Fixer", type:"Blog", author:"Ray", contact:"ray@zhaodao.ai", rss:"https://capray.blog/feed.xml"},
-    {url:"https://modao.cc/map", title:"墨刀", type:"Website"},
-    {url:"https://www.writeathon.cn", title:"Writeathon", type:"Website",contact:"achu@writeathon.cn"},
-    {url:"https://coderplanets.com", title:"coderplanets", type:"Website", author:"mydearxym", contact: "mydearxym@qq.com"},
-    {url:"https://mianbaoduo.com", title:"面包多", type:"Website", author:"dk", contact: "dk@niucodata.com"},
-    {url:"https://www.barretlee.com/entry/", title:"小胡子哥的个人网站", type:"Website", author:"barretlee", contact: "barret.china@gmail.com"},
-    {url:"https://yulinfeng.cnblogs.com", title:"CoderBuff", type:"WebSite", author:"OKevin", contact:"hellobug@outlook.com"},
-    {url:"https://hufangyun.com", title:"小猿大圣的博客", type:"WebSite", author:"Youthink", contact:"hufy78@gmail.com", rss:"https://hufangyun.com/atom.xml"},
-    {url:"https://indiehackers.net", title:"独立开发者", type:"WebSite", author:"cmlanche", contact:"cmlanche@gmail.com", rss:"https://indiehackers.net/topics/feed"},
-    {url:"https://hiwannz.com/", title:"见字如面", type:"WebSite", author:"wannz", contact:"infante4@foxmail", rss:"https://hiwannz.com/feed"},
-    {url:"https://www.ouorz.com", title:"TonyHe 的博客", type:"WebSite", author:"TonyHe", contact:"quickbrown_fox@outlook.com", rss:"https://blog.ouorz.com/feed"},
-    {url:"https://jike.xyz", title:"极客", type:"WebSite", author:"Hosea", contact:"hosea@hsu.pw"},
+    {url:"https://ldd.cc", title:"卢东东", type:"Website", contact:"xjoylu@gmail.com", rss:"https://ldd.cc/rss"},
+    {url:"https://lcc.cc/sunny", title:"Sunny.H", type:"Blog", contact:"233hjs@gmail.com"},
 ];
